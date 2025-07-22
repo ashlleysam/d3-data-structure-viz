ab67496a02e1cee33bdf1c4e4ed83f43300a2c75
ashlleysam <8015834+ashlleysam@users.noreply.github.com>
2025-07-22 00:29:04 -0500

    feat: Add context menu item for editing node

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function recomputeSpacing(nodes, links) {
   // Recursively compute the width needed to ensure
   // that the subtrees don't overlap
   function get_widths(id) {
-    let node = nodeById.get(id);
+    let par_node = nodeById.get(id);
     if (leftChild.has(id)) {
       get_widths(leftChild.get(id));
     }
@@ -88,31 +88,60 @@ function recomputeSpacing(nodes, links) {
     }
     let left = leftChild.has(id) ? nodeById.get(leftChild.get(id)) : null;
     let right = rightChild.has(id) ? nodeById.get(rightChild.get(id)) : null;
-    node.bst_width_left = 0;
-    node.bst_width_right = 0;
+    par_node.bst_width_left = 0;
+    par_node.bst_width_right = 0;
     // If there is a left or right child, the parent needs
     // enough space to hold that child's subtree plus some padding
     if (left != null) {
-      node.bst_width_left = NODE_SEP_X + left.bst_width;
+      par_node.bst_width_left = NODE_SEP_X + left.bst_width;
     }
     if (right != null) {
-      node.bst_width_right = NODE_SEP_X + right.bst_width;
+      par_node.bst_width_right = NODE_SEP_X + right.bst_width;
     }
-    node.bst_width = node.bst_width_left + node.bst_width_right;
+    par_node.bst_width = par_node.bst_width_left + par_node.bst_width_right;
   }
   roots.forEach(get_widths);
 }
 
 var width = window.innerWidth,
     height = window.innerHeight;
-const menu = document.getElementById("contextMenu");
+const context_menu = document.getElementById("contextMenu");
+const context_add_node = document.getElementById("menu-item-add-node");
+const context_delete_node = document.getElementById("menu-item-delete-node");
+const context_edit_node = document.getElementById("menu-item-edit-node");
+let ctx_menu_select_id = null;
 
 function showContextMenu(e) {
-  menu.style = `width: 300px; left: ${e.pageX}px; top: ${e.pageY}px;`;
+  context_menu.style = `width: 300px; left: ${e.pageX}px; top: ${e.pageY}px;`;
+  context_add_node.style = node_hover_id == null ? "" : "display: none;";
+  context_delete_node.style = node_hover_id == null ? "display: none;" : "";
+  context_edit_node.style = node_hover_id == null ? "display: none;" : "";
+  ctx_menu_select_id = node_hover_id;
 }
 
 function hideContextMenu() {
-  menu.style = `width: 0px; left: 0px; top: 0px; display: none;`;
+  context_menu.style = `width: 0px; left: 0px; top: 0px; display: none;`;
+}
+
+context_add_node.onclick = function(e) {
+  hideContextMenu();
+  addNode();
+}
+
+context_delete_node.onclick = function(e) {
+  hideContextMenu();
+  if (ctx_menu_select_id != null) {
+    deleteNode(ctx_menu_select_id);
+    ctx_menu_select_id = null;
+  }
+}
+
+context_edit_node.onclick = function(e) {
+  hideContextMenu();
+  if (ctx_menu_select_id != null) {
+    selectNode(ctx_menu_select_id);
+    ctx_menu_select_id = null;
+  }
 }
 
 let svg = d3.select("#d3_container")
@@ -223,16 +252,6 @@ function redraw() {
 
   var update_nodes = g_node.selectAll(".node").data(nodes);
 
-  // node = update_nodes
-  //   .enter()
-  //   .append("g")
-  //   .attr("class", "node")
-  //   .on("dblclick", node_dblclick)
-  //   .call(d3.drag().on("start", dragstarted).on("drag", dragged).on("end", dragended))
-  //   .on('mouseover', function(e, d) {
-  //     d3.select(this).raise();
-  //   })
-  //   .merge(update_nodes);
   node = update_nodes
     .enter()
     .append("g")
@@ -341,6 +360,56 @@ function addNode() {
   redraw();
 }
 
+function deleteNode(node_id) {
+  nodes = nodes.filter(d => d.id != node_id);
+  bst_edges = bst_edges.filter(d => d.parent.id != node_id && d.child.id != node_id);
+  redraw();
+  if (node_id == node_clicked_id) {
+    data_input.value = "";
+    none_button.checked = false;
+    red_button.checked = false;
+    black_button.checked = false;
+    data_input.disabled = true;
+    none_button.disabled = true;
+    red_button.disabled = true;
+    black_button.disabled = true;
+    node_clicked_id = null;
+  }
+}
+
+function deleteEdge(edge_id) {
+  bst_edges = bst_edges.filter(d => d.id != edge_id);
+  redraw();
+}
+
+function selectNode(node_id) {
+  if (node_clicked_id != null) {
+    nodeById.get(node_clicked_id).selected = false;
+  }
+  let node_data = nodeById.get(node_id);
+  node_data.selected = true;
+
+  node.selectAll(".nodeShape").attr("stroke-width", d => d.selected ? "4px" : "0px");
+
+  node_clicked_id = node_id;
+  
+  data_input.value = node_data.label;
+  none_button.checked = false;
+  red_button.checked = false;
+  black_button.checked = false;
+  if (node_data.color == RED) {
+    red_button.checked = true;
+  } else if (node_data.color == BLACK) {
+    black_button.checked = true;
+  } else {
+    none_button.checked = true;
+  }
+  none_button.disabled = false;
+  red_button.disabled = false;
+  black_button.disabled = false;
+  data_input.disabled = false;
+}
+
 function deselectNode() {
   if (node_clicked_id == null) return;
   nodeById.get(node_clicked_id).selected = false;
@@ -356,11 +425,6 @@ function deselectNode() {
   data_input.value = "";
 }
 
-document.getElementById("menu-item-add-node").onclick = function(e) {
-  hideContextMenu();
-  addNode();
-}
-
 d3.select("body")
   .on("keydown", function(e) {
     if (e.key == ESCAPE) {
@@ -369,25 +433,10 @@ d3.select("body")
       addNode();
     } else if (e.key == 'd') {
       if (node_hover_id != null) {
-        nodes = nodes.filter(d => d.id != node_hover_id);
-        bst_edges = bst_edges.filter(d => d.parent.id != node_hover_id && d.child.id != node_hover_id);
-        redraw();
-        // console.log(node_hover_id, node_clicked_id);
-        if (node_hover_id == node_clicked_id) {
-          data_input.value = "";
-          none_button.checked = false;
-          red_button.checked = false;
-          black_button.checked = false;
-          data_input.disabled = true;
-          none_button.disabled = true;
-          red_button.disabled = true;
-          black_button.disabled = true;
-          node_clicked_id = null;
-        }
+        deleteNode(node_hover_id);
         node_hover_id = null;
       } else if (edge_hover_id != null) {
-        bst_edges = bst_edges.filter(d => d.id != edge_hover_id);
-        redraw();
+        deleteEdge(edge_hover_id);
         edge_hover_id = null;
       }
     } else if (e.key == "Control") {
@@ -403,9 +452,10 @@ d3.select("body")
     // Always hide the context menu on the next click
     hideContextMenu();
 
-    // If there is a selected node, deselect it if the click
-    // is not on the edit menu or the node itself
+    // If there is a selected node, deselect it if the click isn't on either
+    // the data menu, the edit context menu, or on the node itself
     if (data_menu.contains(e.target)) return;
+    if (context_edit_node.contains(e.target)) return;
     if (node_clicked_id == null) return;
     let sel_node = node.filter(d => d.id == node_clicked_id).nodes()[0];
     if (sel_node.contains(e.target)) return;
@@ -413,30 +463,7 @@ d3.select("body")
   });
 
 function node_dblclick(event, d) {
-  if (node_clicked_id != null) {
-    nodeById.get(node_clicked_id).selected = false;
-  }
-  d.selected = true;
-
-  node.selectAll(".nodeShape").attr("stroke-width", d => d.selected ? "4px" : "0px");
-
-  node_clicked_id = d.id;
-
-  data_input.value = d.label;
-  none_button.checked = false;
-  red_button.checked = false;
-  black_button.checked = false;
-  if (d.color == RED) {
-    red_button.checked = true;
-  } else if (d.color == BLACK) {
-    black_button.checked = true;
-  } else {
-    none_button.checked = true;
-  }
-  none_button.disabled = false;
-  red_button.disabled = false;
-  black_button.disabled = false;
-  data_input.disabled = false;
+  selectNode(d.id);
 }
 
 function node_onmouseover(event, d) {
